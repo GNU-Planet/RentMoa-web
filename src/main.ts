6e5386cd94d4ae188f9d0dec4c94a2f6bea18d31
Jang-DongHo <donghoo18@naver.com>
2023-03-21 21:48:06 +0900

    [Feat] Pug 설정

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3000);
   // pug 설정
-  app.useStaticAssets(join(__dirname, '..', 'assets'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 }
